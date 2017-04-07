6a3caf43240115a9fc01aff44f934a7339b5abfe
zcox2 <zacharycox@Zachs-MacBook-Pro.local>
2017-04-07 16:23:28 -0400

    Finally fixed constraints by adding buttonStackWidth = liftStackWidth

--- LiftingApp/WorkoutTableViewController.swift
+++ LiftingApp/WorkoutTableViewController.swift
@@ -75,6 +75,7 @@ class WorkoutTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
